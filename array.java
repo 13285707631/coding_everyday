c22773db161c0eca95b4b3ea4a4c746170536c8a
邹迎童 <30305045+13285707631@users.noreply.github.com>
2020-03-26 14:54:37 +0800

    Update array.java

--- array.java
+++ array.java
@@ -5,7 +5,7 @@ public class array {
     }
     static boolean Find(int target, int [][] array) {
         int hang=array.length-1;
-        if(array.length==0)
+        if(array.length==0)//因为是二维的数组所以必须要有这样的一个检测判断
             return false;
         if(array[0].length==0)
             return false;
@@ -24,3 +24,16 @@ public class array {
     }
 
 }
+
+
+利用该二维数组的性质：
+
+每一行都按照从左到右递增的顺序排序，
+每一列都按照从上到下递增的顺序排序
+改变个说法，即对于左下角的值 m，m 是该行最小的数，是该列最大的数
+每次将 m 和目标值 target 比较：
+
+当 m < target，由于 m 已经是该列最大的元素，想要更大只有从行考虑，取值右移一位
+当 m > target，由于 m 已经是该行最小的元素，想要更小只有从列考虑，取值上移一位
+当 m = target，找到该值，返回 true
+用某行最小或某列最大与 target 比较，每次可剔除一整行或一整列
