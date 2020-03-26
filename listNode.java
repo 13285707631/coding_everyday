6d81848a5f2183148dea4b42c3bf82686a2c7262
邹迎童 <30305045+13285707631@users.noreply.github.com>
2020-03-26 15:55:48 +0800

    Add files via upload

---
+++ listNode.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+class ListNode {
+         int val;
+         ListNode next = null;
+
+        ListNode(int val) {
+             this.val = val;
+         }
+     }
+
+
+public class listNode {
+     //非递归，使用ArrayList.add的后移操作
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+            ArrayList<Integer> list=new ArrayList<>();
+            ListNode tmp=listNode;
+            while(tmp!=null){
+                list.add(0,tmp.val);
+                tmp=tmp.next;
+            }
+            return list;
+    }
+    //利用栈
+    public ArrayList printListFromTailToHead2(ListNode listNode) {
+        ArrayList list=new ArrayList<>();
+        Stack stack=new Stack();
+        ListNode listNode1=listNode;
+        while(listNode1!=null){
+            stack.push(listNode1.val);
+            listNode1=listNode1.next;
+        }
+        while(!stack.empty()){
+            list.add(stack.pop());
+        }
+        return list;
+    }
+
+}
